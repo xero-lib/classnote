d65c8561998cf345f4ff03cdd8b7a0623af83548
Thoth <jonathankurtz@protonmail.com>
2026-02-08 20:13:51 -0600

    feat: more logical Times enum

--- crates/data/src/class.rs
+++ crates/data/src/class.rs
@@ -1,32 +1,35 @@
 use std::fmt::Display;
 
-use super::time::ClassTime;
+use super::time::Times;
 
 use serde::{Serialize, Deserialize};
 
 #[derive(Debug, Serialize, Deserialize, Default, Clone)]
 pub struct Class {
     name: String,
-    times: Vec<ClassTime>,
+    times: Times,
     professor: String,
-    office_hours: Vec<ClassTime>
+    office_hours: Times 
 }
 
 impl Display for Class {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.write_str(
-            &self.times.iter().fold(String::new(), |init, time| {
-                init + &format!("\t{name} ({prof}) on {day}, from {start} to {end}, at {camp}, {bldg} (Room {room})\n",
-                    name  = self.name,
-                    prof  = self.professor,
-                    camp  = time.location.campus,
-                    bldg  = time.location.building,
-                    room  = time.location.room,
-                    start = time.start.hms_string(),
-                    end   = time.end.hms_string(),
-                    day   = time.start.day
+            match &self.times {
+                Times::Async => format!("\t{} ({}), asynchronous.", self.name, self.professor),
+                Times::At(times) => times.iter().fold(String::new(), |init, time|
+                    format!("{init}\t{name} ({prof}) on {day}, from {start} to {end}, at {camp}, {bldg} (Room {room})\n",
+                        name  = self.name,
+                        prof  = self.professor,
+                        camp  = time.location.campus,
+                        bldg  = time.location.building,
+                        room  = time.location.room,
+                        start = time.start.hms_string(),
+                        end   = time.end.hms_string(),
+                        day   = time.start.day
+                    )
                 )
-            })
+            }.as_str()
         )
     }
 }
@@ -40,7 +43,7 @@ impl Class {
         }        
     }
 
-    pub fn new(name: String, professor: String, times: Vec<ClassTime>, office_hours: Vec<ClassTime>) -> Class {
+    pub fn new(name: String, professor: String, times: Times, office_hours: Times) -> Class {
          Class {
             name,
             times,
@@ -57,7 +60,7 @@ impl Class {
         &self.professor
     }
 
-    pub fn get_times(&self) -> &Vec<ClassTime> {
+    pub fn get_times(&self) -> &Times {
         &self.times
     }
 }
